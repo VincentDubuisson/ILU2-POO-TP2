c6494999805ce6446210e3242ddeab9e7188add9
VincentDubuisson <vincent.it.dubuisson@gmail.com>
2024-03-13 11:49:59 +0100

    tp2-4

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,10 +13,46 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		boolean etalDisponible;
+		
+		if (!(controlPrendreEtal.verifierIdentite(nomVendeur))) {
+			System.out
+			.println("Je suis désolé " + nomVendeur + " mais il faut être un "
+					+ "habitant de notre village pour commercer ici\n");
+		} else {
+			System.out
+			.println("Bonjour " + nomVendeur + " ,je vais regarder si je peux vous trouver un étal.\n");
+			etalDisponible = controlPrendreEtal.resteEtals();
+			
+			if (!(etalDisponible)) {
+				System.out
+				.println("Désolé " + nomVendeur + "je n'ai plus d'étal qui ne soit pas déjà occupé.\n");
+				
+			} else {
+				installerVendeur(nomVendeur);
+				
+			}
+			
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		
+		StringBuilder chaine = new StringBuilder();
+		chaine.append("C'est parfait, il me reste un étal pour vous !\n");
+		chaine.append("Il me faudrait quelques renseignements :\n");
+		chaine.append("Quel produit souhaitez-vous vendre ?");
+		System.out.println(chaine.toString());
+		String produit = scan.next();
+		
+		StringBuilder question = new StringBuilder();
+		question.append("Combien souhaitez-vous en vendre ?");
+		int nbProduit = Clavier.entrerEntier(question.toString());
+		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		
+		if (numeroEtal != -1) {
+			System.out
+				.println("Le vendeur " + nomVendeur + " s'est installé à l'étal n°" + (numeroEtal + 1));
+		}
 	}
 }
